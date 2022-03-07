5c159bfb50f606fb3a2dbbb8485c71505be947ee
Israel Pérez Meléndez <iperezmel78@gmail.com>
2022-03-07 18:16:04 -0500

    Update buildApp.groovy

--- vars/buildApp.groovy
+++ vars/buildApp.groovy
@@ -2,7 +2,7 @@
 
 def call(project, app){
     projectSet(project)
-    sh "oc new-build -n ${project} --binary --name=${app} -l app=${app} --build-arg --build-arg NPM_CA_FILE=nexus.crt || echo 'Build exists'"
+    sh "oc new-build -n ${project} --binary --name=${app} -l app=${app} --build-arg NPM_CA_FILE=nexus.crt || echo 'Build exists'"
     sh "oc start-build ${app} -n ${project} --from-dir=. --follow"
     deployApp(project, app, 'latest')
 }
