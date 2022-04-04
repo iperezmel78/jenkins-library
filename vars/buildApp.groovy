d49f6aa4e74d98b9107ff9d770990eff71d5a2ba
Israel Pérez Meléndez <iperezmel78@gmail.com>
2022-04-04 12:36:24 -0500

    Update buildApp.groovy

--- vars/buildApp.groovy
+++ vars/buildApp.groovy
@@ -4,5 +4,6 @@ def call(project, app){
     projectSet(project)
     sh "oc new-build -n ${project} --binary --name=${app} -l app=${app} --build-arg NPM_AUTH=${NPM_AUTH} --build-arg NPM_CA_FILE=nexus.crt || echo 'Build exists'"
     sh "oc start-build ${app} -n ${project} --from-dir=. --follow"
+    sh "oc patch dc/${app} -p '[{\"op\":\"add\",\"path\":\"/spec/template/spec/containers/0/envFrom\",\"value\":[{\"secretRef\":{\"name\":\"mysqldb\"}}]}]' --type=json"
     deployApp(project, app, 'latest')
 }
