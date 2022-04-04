cf67b3773849de28c0dc376dee5d221ca056cb64
Israel Pérez Meléndez <iperezmel78@gmail.com>
2022-04-04 13:06:53 -0500

    Update canaryDeploy.groovy

--- vars/canaryDeploy.groovy
+++ vars/canaryDeploy.groovy
@@ -5,5 +5,5 @@ def call(project, app, version){
     sh "oc set probe -n ${project} dc/${app}-${version} --readiness --get-url=http://:8080/check"
     sh "oc patch svc/${app} -n ${project} -p '{\"spec\":{\"selector\":{\"app\": \"${app}\", \"deploymentconfig\": null}, \"sessionAffinity\":\"None\"}}' || echo 'Service ${app} already patched'"
     sh "oc delete service ${app}-${version} -n ${project}"
-    sh "oc patch dc/${app} -p '[{\"op\":\"add\",\"path\":\"/spec/template/spec/containers/0/envFrom\",\"value\":[{\"secretRef\":{\"name\":\"mysqldb\"}}]}]' --type=json"
+    sh "oc patch dc/${app}-${version} -p '[{\"op\":\"add\",\"path\":\"/spec/template/spec/containers/0/envFrom\",\"value\":[{\"secretRef\":{\"name\":\"mysqldb\"}}]}]' --type=json"
 }
