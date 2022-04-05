e10e124f546747592e02f41c5161e4159f3fadab
Israel Pérez Meléndez <iperezmel78@gmail.com>
2022-04-05 05:35:18 -0500

    Update deployApp.groovy

--- vars/deployApp.groovy
+++ vars/deployApp.groovy
@@ -4,5 +4,4 @@ def call(project, app, tag){
     sh "oc new-app --name=${app} -n ${project} -l app=${app},hystrix.enabled=true --image-stream=${project}/${app}:${tag} || echo 'Aplication already Exists'"
     sh "oc expose service ${app} -n ${project} || echo 'Service already exposed'"
     sh "oc set probe dc/${app} -n ${project} --readiness --get-url=http://:8080/check"
-    sh "oc patch dc/${app} -p '[{\"op\":\"add\",\"path\":\"/spec/template/spec/containers/0/envFrom\",\"value\":[{\"secretRef\":{\"name\":\"mysqldb\"}}]}]' --type=json"
 }
