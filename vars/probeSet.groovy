a2601f49a3551cd3855ec52b2abf75716fba3fd4
Israel Pérez Meléndez <iperezmel78@gmail.com>
2023-12-10 01:11:23 -0500

    Delete vars/probeSet.groovy
    update

--- vars/probeSet.groovy
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/groovy
-
-def call(project, app){
-    sh "oc set probe dc/${app} -n ${project} --readiness --get-url=http://:8080/check"
-    sh "oc set probe dc/${app} -n ${project} --liveness --get-url=http://:8080/check"
-}
