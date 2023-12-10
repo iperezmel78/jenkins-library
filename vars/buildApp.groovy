b2ae89a7969a938ca71e8a722603737233f50555
Israel Pérez Meléndez <iperezmel78@gmail.com>
2023-12-10 00:48:07 -0500

    Update buildApp.groovy
    update

--- vars/buildApp.groovy
+++ vars/buildApp.groovy
@@ -1,8 +1,6 @@
 #!/usr/bin/groovy
 
 def call(project, app){
-    projectSet(project)
     sh "oc new-build -n ${project} --binary --name=${app} -l app=${app} --build-arg NPM_AUTH=${NPM_AUTH} --build-arg NPM_CA_FILE=nexus.crt || echo 'Build exists'"
     sh "oc start-build ${app} -n ${project} --from-dir=. --follow"
-    deployApp(project, app, 'latest')
 }
