8b5186746a7556a9ebc695ecee5ffdb2d5a11bf2
Israel Pérez Meléndez <iperezmel78@gmail.com>
2023-12-11 00:54:53 -0500

    Update deployApp.groovy
    update

--- vars/deployApp.groovy
+++ vars/deployApp.groovy
@@ -3,6 +3,6 @@
 def call(project, app, tag){
     sh "oc new-app --name=${app} -n ${project} -l app=${app},hystrix.enabled=true --image-stream=${project}/${app}:${tag} || echo 'Aplication already Exists'"
     sh "oc expose service ${app} -n ${project} || echo 'Service already exposed'"
-    sh "oc set probe deployment/${app} -n ${project} --readiness --get-url=http://:8080/check"
-    sh "oc set probe deployment/${app} -n ${project} --liveness --get-url=http://:8080/check"
+    sh "oc set probe dc/${app} -n ${project} --readiness --get-url=http://:8080/check"
+    sh "oc set probe dc/${app} -n ${project} --liveness --get-url=http://:8080/check"
 }
